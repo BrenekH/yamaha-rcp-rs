ee7f46b1de56f86b0214bfc3d11f6a7c45fbc70d
Brenek Harrison <brenekharrison@gmail.com>
2023-02-16 23:31:10 -0700

    Connect to OBS-WS

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use obws::Client;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Connect to the OBS instance through obs-websocket.
+    let client = Client::connect("localhost", 4455, None::<String>).await?;
+
+    // Get and print out version information of OBS and obs-websocket.
+    let version = client.general().version().await?;
+    println!("{:#?}", version);
+
+    // Get a list of available scenes and print them out.
+    let scene_list = client.scenes().list().await?;
+    println!("{:#?}", scene_list);
+
+    Ok(())
+}
